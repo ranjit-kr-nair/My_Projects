ef84b8c87f1809946e4c392fbe3c2934866ad934
Ranjit Krishnan Nair <ranjit.kr.nair@gmail.com>
2023-03-30 22:51:41 +0530

    Update Test_vs_Control_P_Value.py

--- p_Value/Test_vs_Control_P_Value.py
+++ p_Value/Test_vs_Control_P_Value.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import scipy.stats as stats
 
+# Import data from CSV file
+data = pd.read_csv('data.csv')
+
+# Split data into test and control groups
+test = data[data['group'] == 'test']
+control = data[data['group'] == 'control']
+
+# Calculate conversion rates for test and control groups
+test_conv_rate = test['did the user convert'].mean()
+control_conv_rate = control['did the user convert'].mean()
+
+# Calculate the probability of the test group beating the control group
+p_value = stats.ttest_ind(test['did the user convert'], control['did the user convert']).pvalue / 2  # Divide by 2 for one-tailed test
+test_beats_control_prob = 1 - p_value
+
+print('Test conversion rate:', test_conv_rate)
+print('Control conversion rate:', control_conv_rate)
+print('Probability of test beating control:', test_beats_control_prob)
